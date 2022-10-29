962d662cf82e553361a5801844b04659189511ed
Msk142 <khjun7830@gmail.com>
2022-10-29 16:22:15 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -12,10 +12,21 @@ function App() {
 
   const Login = details => {
     console.log(details);
+
+    if(details.email == adminUser.email && details.password == adminUser.password){
+      console.log("Logged in");
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+    }else{
+
+      console.log("Details do not match!");
+    }
   }
 
   const Logout = () => {
-    console.log("Logout");
+    setUser({name:"", email:""})
   }
 
   return (
@@ -23,7 +34,7 @@ function App() {
       {(user.email != "") ? (
         <div>
         <h2>Welcome, <span>{user.name}</span></h2>
-        <button>Logout</button>
+        <button onClick={Logout}>Logout</button>
         </div>
       ) : (
         <LoginForm Login={Login} error={error}/>
