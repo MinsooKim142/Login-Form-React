8ec8ad799081c6cb3ac090b8e3a2601baab6f3c1
Msk142 <khjun7830@gmail.com>
2022-10-29 16:25:48 -0400

    error set

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -14,7 +14,7 @@ function LoginForm({Login, error}) {
     <form onSubmit = {submitHandler}>
         <div className="form-inner">
             <h2>Login</h2>
-            {/* Error! */}
+            {(error != "") ? ( <div className="error">{error}</div>) : ""}
 
             <div className = "form-group">
                 <label htmlFor="name">Name: </label>
